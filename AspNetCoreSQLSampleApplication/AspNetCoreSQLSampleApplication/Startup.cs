4f2cfe795cc81058399de96539da5594bbd499e8
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2020-08-31 09:28:33 +0200

    extend timeout for session

--- AspNetCoreSQLSampleApplication/AspNetCoreSQLSampleApplication/Startup.cs
+++ AspNetCoreSQLSampleApplication/AspNetCoreSQLSampleApplication/Startup.cs
@@ -33,7 +33,7 @@ namespace AspNetCoreSQLSampleApplication
 
             services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
